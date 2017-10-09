1d7e91bcbcf8a1ae356413136df3e1272b179be4
parsav.jain <parsav.jain@wipro.com>
2017-10-09 16:45:59 +0530

    Initial commit

--- src/com/stackroute/datamunger/DataMunger.java
+++ src/com/stackroute/datamunger/DataMunger.java
@@ -1,31 +1,43 @@
 package com.stackroute.datamunger;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class DataMunger {
 
-	public static void main(String[] args) {
+	// Regex for white spcae.
+	private static final String WHITE_SPACE = "\\S+";
 
+	public static void main(String[] args) throws IOException {
 
 		/* read a sentence from the user */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String string = br.readLine();
 
 		/*
-		 * call getWordCount() method which should return no. of words present
-		 * in the string and display the same
+		 * call getWordCount() method which should return no. of words present in the
+		 * string and display the same
 		 */
+		DataMunger dataMunger = new DataMunger();
+		System.out.println(dataMunger.getWordCount(string));
 
 	}
 
-
-	/*
-	 * This method is used to calculate the no. of words present in the given
-	 * string. Please note that in a sentence, words are split by space. Please change the method return 
-	 * type to int
+	/**
+	 * Method to count number of words in a sentence.
+	 *
+	 * @param sentence
+	 *
+	 * @return wordCount
 	 */
-	public Object getWordCount(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	public int getWordCount(String string) {
+		// Checking whether sentence is null or empty.
+		if (null == string || string.trim().isEmpty())
+			return 0;
+
+		// Splitting sentence based on white spaces.
+		return string.split(WHITE_SPACE).length;
 	}
-	
 
 }
